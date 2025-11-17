02c94ad6aecee81507d31a8a0577ba61830aa573
준오 <juno@jun-oui-noteubug.local>
2025-11-17 09:24:13 +0900

    delete cahsed

--- mission3/API/article/article.service.js
+++
@@ -1,8 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
-
-export const findUniqueArticle = async (id) => {
-  return prisma.article.findUnique({
-    where: { id },
-  });
-};
--- mission3/API/product/product.service.js
+++
@@ -1,6 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
-
-export const getUniqueProductById = async (id) => {
-  return await prisma.product.findUnique({where:{id}});
-};
