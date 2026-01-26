b1c462f4e9c8c13061f88a42127db34c91d8024d
준오 <juno@jun-oui-noteubug.local>
2026-01-26 18:15:39 +0900

    feat: push method

--- algorithm/stack.ts
+++ algorithm/stack.ts
@@ -1,19 +1,25 @@
-class stack<T>{
-    items : T[] = [];
-    capacity : number;
+class stack<T> {
+  items: T[] = [];
+  capacity: number;
 
-    constructor(capacity : number){
-        this.capacity =  capacity
+  constructor(capacity: number) {
+    this.capacity = capacity;
+  }
+  peak() {
+    if (this.items.length !== 0) {
+      return this.items[-1];
     }
-    peak(){
-        if(this.items.length !== 0){
-            return this.items[-1]
-        }
-        return ;
+    return;
+  }
+  isEmpty() {
+    return this.items.length === 0;
+  }
+  //pop(){}
+  push(value:T) {
+    if (this.items.length >= 0) {
+      return;
+    } else {
+        this.items.push(value)
     }
-    isEmpty(){
-        return this.items.length === 0
-    }
-    //pop(){}
-    //push(){}
+  }
 }
