08d6d9985a80ba4353f5017b7d34da20460f3fad
준오 <juno@jun-oui-noteubug.local>
2026-01-26 17:41:14 +0900

    feat: peak and is Empty methods

---
+++ algorithm/stack.ts
@@ -0,0 +1,19 @@
+class stack<T>{
+    items : T[] = [];
+    capacity : number;
+
+    constructor(capacity : number){
+        this.capacity =  capacity
+    }
+    peak(){
+        if(this.items.length !== 0){
+            return this.items[-1]
+        }
+        return ;
+    }
+    isEmpty(){
+        return this.items.length === 0
+    }
+    //pop(){}
+    //push(){}
+}
