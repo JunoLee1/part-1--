094c02ba48b9ba49a14b3ccdb6bea877be5c4e79
준오 <juno@jun-oui-noteubug.local>
2026-01-27 14:02:15 +0900

    feat: method isFull, push, logging

--- algorithm/stack.ts
+++ algorithm/stack.ts
@@ -1,3 +1,4 @@
+console.log("🔥 FILE EXECUTED 🔥")
 class stack<T> {
   items: T[] = [];
   capacity: number;
@@ -5,21 +6,37 @@ class stack<T> {
   constructor(capacity: number) {
     this.capacity = capacity;
   }
-  peak() {
+  peek(): T | undefined {
+    console.log(1)
     if (this.items.length !== 0) {
-      return this.items[-1];
+      return this.items[this.items.length - 1];
     }
     return;
   }
-  isEmpty() {
+  isEmpty(): boolean {
     return this.items.length === 0;
   }
-  //pop(){}
-  push(value:T) {
-    if (this.items.length >= 0) {
+  pop(): T | undefined {
+    if (this.isEmpty()) return;
+    return this.items.pop();
+  }
+  isFull():boolean {
+    return this.items.length === this.capacity
+  }
+  push(value: T) {
+    console.log(12)
+    if (this.isFull()) {
       return;
     } else {
-        this.items.push(value)
+      this.items.push(value);
     }
   }
 }
+  const s = new stack<number>(5)
+  console.log(s.isEmpty())
+  s.push(1)
+  s.push(2)
+  s.push(3)
+  s.push(4)
+  s.push(5)
+  console.log(s.peek())
